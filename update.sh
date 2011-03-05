0fb737a5ef20564dba22eb149a746c5a574a2ac7
joe di castro <joe@joedicastro.com>
2011-03-05 13:51:43 +0100

    Separate template and config files for each planet due problems with planet dates.

--- update.sh
+++ update.sh
@@ -2,11 +2,11 @@
 
 cp ./plugins/status-log.py.es ./plugins/status-log.py
 cd ./es
-LANG=es_ES.utf8 rawdog -c config.es -uw -d .
+LANG=es_ES.utf8 rawdog -uw -d .
 
 cd ..
 
 cp ./plugins/status-log.py.en ./plugins/status-log.py
 cd ./en
-LANG=en_GB.utf8 rawdog -c config.en -uw -d .
+LANG=en_GB.utf8 rawdog -uw -d .
 
